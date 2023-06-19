e53b49e1c1a4211a49eff989b4d46059ecd15183
Hudson Carolino <hudson@moredev.tech>
2023-06-19 17:25:03 -0300

    feat: adding confg global style-component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import GlobalStyle from './index.ts'
 import { ThemeProvider } from 'styled-components';
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
