c231ededb6c4cfaf21e94200f6382e9e27873bcd
Hudson Carolino <hudson@moredev.tech>
2023-06-20 15:35:14 -0300

    fix: Component property adjustment

--- src/component/ProductList/index.tsx
+++ src/component/ProductList/index.tsx
@@ -3,9 +3,11 @@ import {List} from './style'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Card from '../Card';
-import Image from './../../assets/image.png'
 
-const ProductList = () => {
+
+const ProductList = (props) => {
+    const {books} = props
+    console.log(books)
     return (
         <List>
             <Swiper
@@ -13,56 +15,20 @@ const ProductList = () => {
                 slidesPerView={5}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
-                
                 >
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl={Image} 
-                        author="Viviana Mazza" 
-                        isAvailable={true}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl="" 
-                        author="Viviana Mazza" 
-                        isAvailable={true}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl={Image} 
-                        author="Viviana Mazza" 
-                        isAvailable={false}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl={Image} 
-                        author="Viviana Mazza" 
-                        isAvailable={true}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl="" 
-                        author="Viviana Mazza" 
-                        isAvailable={true}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="O menino Nelson Mandela" 
-                        imgUrl={Image} 
-                        author="Viviana Mazza" 
-                        isAvailable={false}
-                    />
-                </SwiperSlide>
+                {
+                    books.map(({volumeInfo,saleInfo}, index)=>(
+                        <SwiperSlide  key={index}>
+                            <Card 
+                                title={volumeInfo.title}
+                                imgUrl={volumeInfo.imageLinks?.thumbnail}
+                                authors={volumeInfo?.authors}
+                                isAvailable={saleInfo.saleability}
+                                key={index}
+                            />
+                        </SwiperSlide>
+                    ))
+                }
             </Swiper>
         </List>
     )
